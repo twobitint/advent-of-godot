7f916957ca9454b0eaf5e176b91a868f53478fb4
Justin Labak <jtlabak@gmail.com>
2023-11-21 22:36:53 -0800

    proof of concept finished

--- 2021/01/Floor.gd
+++ 2021/01/Floor.gd
@@ -1,20 +1,28 @@
 extends Node2D
 
-@export var _texture: Texture2D
+signal bounced(depth)
+
+@export var _texture1: Texture2D
+@export var _texture2: Texture2D
 
 var _starting_depth = null
 var _floor = []
+var _fnl = FastNoiseLite.new()
+
+func scan(x):
+    x -= position.x - 50
+    return _floor[x / 16.0]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    var file = FileAccess.open('res://2021/01/test.txt', FileAccess.READ)
+    var file = FileAccess.open('res://2021/01/input.txt', FileAccess.READ)
     var input = file.get_as_text()
 
     var lines = input.split('\n')
     var _starting_depth = int(lines[0])
     
     # Prep left-padding
-    for x in range(0, 20):
+    for x in range(0, 50):
         _floor.append(_starting_depth)
         
     for line in lines:
@@ -26,4 +34,11 @@ func _process(_delta):
 
 func _draw():
     for x in len(_floor):
-        draw_texture(_texture, Vector2(x * 16, _floor[x]))
+        draw_texture(_texture(x), Vector2(x * 16, _floor[x]))
+
+func _texture(x):
+    if int(_fnl.get_noise_1d(x) * 100) % 2 == 0:
+        return _texture1
+    else:
+        return _texture2
+
--- 2021/01/Sub.gd
+++ 2021/01/Sub.gd
@@ -1,10 +1,32 @@
-extends RigidBody2D
+extends CharacterBody2D
 
+var _last_depth = null
+var _increase_counter = 0
+
+var _speed = 20000
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    apply_force(Vector2.RIGHT * 10000)
+    pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-    pass
+func _process(delta):
+    var depth = get_node("/root/DayOne/Floor").scan(position.x)
+    $Label.text = "Depth: %d" % depth
+    get_node("/root/DayOne/Canvas/Output").depth = depth
+    
+    if _last_depth == null:
+        _last_depth = depth
+        
+    if _last_depth < depth:
+        _increase_counter += 1
+        get_node("/root/DayOne/Canvas/Output").count = _increase_counter
+    
+    var target = depth - 50
+    
+    velocity = position.direction_to(Vector2(position.x + 200, target)) * delta * _speed
+        
+    # move_toward()
+        
+    move_and_slide()
+    _last_depth = depth
