f546158ae9b8005cecaad351611732883b896f60
Justin Labak <jtlabak@gmail.com>
2023-11-22 08:57:05 -0800

    cleaner 21d1

--- 2021/01/Sub.gd
+++ 2021/01/Sub.gd
@@ -8,28 +8,17 @@ var _increases = 0
 
 const SPEED = 40000 
 const FLOAT = 50 # Try to stay this far above floor
-const WIDTH = 16 # Scanner resolution width
+const HEADING = 200 # Forward target
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-    var target_depth = _floor.scan(position.x)
+    var target_depth = _floor.scan(position.x) - FLOAT
     _output.depth = target_depth
     
-    # if _last_depth == null:
-    #     _last_depth = depth
-        
-    # if _last_depth < depth:
-    #     _increases += 1
-    #     _output.count = _increases
-    
-    var target = target_depth - FLOAT
-    velocity = position.direction_to(Vector2(position.x + 200, target)) * delta * SPEED
+    var target = Vector2(position.x + HEADING, target_depth)
+    velocity = position.direction_to(target) * delta * SPEED
         
     var x0 = position.x
-        
     move_and_slide()
-    # _last_depth = depth
-
     var x1 = position.x
     
     for x in range(x0, x1):
